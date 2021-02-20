e02ead5d3822aa2bbe28636c21f25af405d14f94
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-20 13:20:22 +0300

    Добавлены решения заданий с 4-6

--- Program.cs
+++ Program.cs
@@ -35,6 +35,51 @@ namespace HomeWork
             int remainImage = imageCount % 3;
             Console.WriteLine(fullRowsCount);
             Console.WriteLine(remainImage);
+            /* ДЗ № 4.
+             * Легенда:
+            Вы приходите в магазин и хотите купить за своё золото кристаллы. 
+            В вашем кошельке есть какое-то количество золота, продавец спрашивает у вас, сколько кристаллов вы хотите купить?
+            После сделки у вас остаётся какое-то количество золота и появляется какое-то количество кристаллов.
+
+            Формально:
+
+            При старте программы пользователь вводит начальное количество золота.
+            Потом ему предлагается купить какое-то количество кристаллов по цене N(задать в программе самому).
+            Пользователь вводит число и его золото конвертируется в кристаллы. Остаток золота и кристаллов выводится на экран. */
+            int goldCount = 100;
+            int gemCost = 4;
+            Console.WriteLine("Сколько кристаллов хотите приобрести?");
+            int gemsToBuyValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"У вас: {goldCount - gemCost * gemsToBuyValue} золота и {gemsToBuyValue} кристалов");
+            Console.WriteLine(@"У вас: {0} золота и {1} кристалов", goldCount - gemCost * gemsToBuyValue , gemsToBuyValue);
+            /*ДЗ № 5.
+             * Легенда:
+
+            Вы заходите в поликлинику и видите огромную очередь из старушек, вам нужно рассчитать время ожидания в очереди.
+
+            Формально:
+            Пользователь вводит кол-во людей в очереди.
+            Фиксированное время приема одного человека всегда равно 10 минутам.
+            Пример ввода: Введите кол-во старушек: 14
+            Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут." */
+            Console.WriteLine("Сколько людей в очереди?");
+            int peopleInQueue = Convert.ToInt32(Console.ReadLine());
+            int timeForPatient = 10;
+            Console.WriteLine(@"Вы должны отстоять в очереди {0} часа и {1} минут", peopleInQueue * timeForPatient / 60, peopleInQueue * timeForPatient % 60);
+            /*ДЗ № 6.
+             * Вы задаете вопросы пользователю, по типу "как вас зовут", "какой ваш знак зодиака" и тд, 
+             * после чего, по данным, которые он ввел, формируете небольшой текст о пользователе.
+
+            "Вас зовут Алексей, вам 21 год, вы водолей и работаете на заводе." */
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Как ваша фамилия?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?");
+            string company = Console.ReadLine();
+            Console.WriteLine("На какой должности?");
+            string post = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {surname} {name}, вы работаете в компании {company} на должности {post}.");
         }
     }
 }
