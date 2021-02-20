7e04cb0df97606cf8fb70e176d96abaaafb390b3
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-20 21:18:23 +0300

    Переделал цикл

---
+++ OutputControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputControl
+{
+    class OutputControl
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            while (true)
+            {
+                Console.WriteLine("Введите пароль или exit для продолжения");
+                userInput = Console.ReadLine();
+                if (userInput == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
