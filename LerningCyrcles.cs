d6804327eb46c3958a081fc3a4473ebb4327e554
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-20 20:16:26 +0300

    Добавил решение по освоению циклов

---
+++ LerningCyrcles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCycles
+{
+    class LerningCyrcles
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше прошение");
+            string inputMessage = Console.ReadLine();
+            Console.WriteLine("Сколько раз повторить ваше прошение?");
+            int repeatMessage = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < repeatMessage; i++)
+            {
+                Console.WriteLine(inputMessage);
+            }
+        }
+    }
+}
