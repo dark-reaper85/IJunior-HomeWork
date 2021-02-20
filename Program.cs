8763fc1b92a51ab5bdfd73f1126f20d7fa3b5339
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-20 12:10:34 +0300

    придумал переменные и проверил задание на вывод

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstModuleVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int equipmentCapacity;
+            byte aggressiveCount;
+            sbyte kindness;
+            uint stepsCount;
+            float mountSpeed;
+            decimal accountBalance;
+            string title;
+            bool isFullInventory;
+            double hipotenuse;
+            char persent;
+
+            int a = 10;
+            int b = 38 * 17;
+            int c = (31 - 5 * a) / b;
+            Console.WriteLine(c);
+
+
+
+
+        }
+    }
+}
