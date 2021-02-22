72bcb136e327e0ca460211ebc02b5fa809d4ecfa
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-22 17:19:16 +0300

    последовательность

---
+++ TheSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSequence
+{
+    class TheSequence
+    {
+        static void Main(string[] args)
+        {
+            int minValue = 7;
+            int maxValue = 98;
+            int stepCount = 7;
+            int value = minValue;
+            while (value <= maxValue)
+            {
+                Console.Write(" " + value);
+                value += stepCount;
+            }
+            Console.WriteLine();
+            for (value = minValue; value <= maxValue; value += stepCount)
+            {
+                Console.Write(" " + value);
+            }
+        }
+    }
+}
