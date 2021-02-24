50de3def56962bb7d2279a5c88743294c0f8dc3e
dark-reaper85 <dark_reaper85@mail.ru>
2021-02-24 11:32:54 +0300

    Доработал и исправил

--- CurrencyConverter.cs
+++ CurrencyConverter.cs
@@ -13,12 +13,13 @@ namespace CurrencyConverter
             float rubBalance = 1000;
             float usdBalance = 100;
             float eurBalance = 100;
-            float rubToUsd = 73.98f;
+            float usdToRub = 73.98f;
             float eurToRub = 89.66f;
             float usdToEur = 0.83f;
             float sumToConvert;
             int inputOperation = 0;
-            while (inputOperation != 7)
+            bool converterWork = true;
+            while (converterWork)
             {
                 Console.Clear();
                 Console.WriteLine($"Ваш баланс: { rubBalance,-5 } рублей");
@@ -42,52 +43,71 @@ namespace CurrencyConverter
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > rubBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        rubBalance -= sumToConvert;
-                        usdBalance += sumToConvert / rubToUsd;
+                        else
+                        {
+                            rubBalance -= sumToConvert;
+                            usdBalance += sumToConvert / usdToRub;
+                        }
                         break;
                     case 2:
                         Console.WriteLine("Какую сумму в долларах хотите обменять?");
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > usdBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        usdBalance -= sumToConvert;
-                        rubBalance += sumToConvert * rubToUsd;
+                        else 
+                        {
+                            usdBalance -= sumToConvert;
+                            rubBalance += sumToConvert * usdToRub;
+                        }
                         break;
                     case 3:
                         Console.WriteLine("Какую сумму в рублях хотите обменять?");
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > rubBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        rubBalance -= sumToConvert;
-                        eurBalance += sumToConvert / eurToRub;
+                        else 
+                        {
+                            rubBalance -= sumToConvert;
+                            eurBalance += sumToConvert / eurToRub;
+                        }
                         break;
                     case 4:
                         Console.WriteLine("Какую сумму в евро хотите обменять?");
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > eurBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        eurBalance -= sumToConvert;
-                        rubBalance += sumToConvert * eurToRub;
+                        else 
+                        {
+                            eurBalance -= sumToConvert;
+                            rubBalance += sumToConvert * eurToRub;
+                        }
                         break;
                     case 5:
                         Console.WriteLine("Какую сумму в долларах хотите обменять?");
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > usdBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        usdBalance -= sumToConvert;
-                        eurBalance += sumToConvert * usdToEur;
+                        else 
+                        {
+                            usdBalance -= sumToConvert;
+                            eurBalance += sumToConvert * usdToEur;
+                        }
                         break;
                     case 6:
                         Console.WriteLine("Какую сумму в евро хотите обменять?");
                         sumToConvert = Convert.ToSingle(Console.ReadLine());
                         if (sumToConvert > eurBalance)
                             Console.WriteLine("Сумма превышает ваш баланс");
-                        eurBalance -= sumToConvert;
-                        usdBalance += sumToConvert / usdToEur;
+                        else 
+                        {
+                            eurBalance -= sumToConvert;
+                            usdBalance += sumToConvert / usdToEur;
+                        }
+                        break;
+                    case 7:
+                        converterWork = false;
                         break;
                     default:
-                        if (inputOperation == 7)
-                            break;
                         Console.WriteLine("Неизвестная операция");
                         break;
                 }
